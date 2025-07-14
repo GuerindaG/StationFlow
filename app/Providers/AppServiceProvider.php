23655bce384f462152de3dbb8a0929d79d410a3e
Guerinda <guerindagohou@gmail.com>
2025-07-14 16:01:14 +0100

    App et Env modification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,11 +4,12 @@ namespace App\Providers;
 
 use App\Models\Categorie;
 use App\Models\Paiement;
-use Auth;
+use App\Models\Station;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
-use App\Models\Station;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,13 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (env('APP_ENV') == 'production') {
+        if (env('APP_ENV') === 'production') {
             $url->forceScheme('https');
         }
+
         Schema::defaultStringLength(191);
+
         View::share('stations', Station::all());
         View::share('paiements', Paiement::all());
         View::share('categories', Categorie::all());
+
         View::composer('*', function ($view) {
             if (Auth::check()) {
                 $view->with('notifications', Auth::user()->unreadNotifications);
